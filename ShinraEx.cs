b58708fd3daf24374b1f340326b6a621632f27e7
RomeroEJ <theonn@x6tencial.net>
2019-07-09 22:05:12 -0500

    Syncing Changes Back to Compilable

--- ShinraEx.cs
+++ ShinraEx.cs
@@ -191,10 +191,6 @@ namespace ShinraCo
                     return new Warrior();
                 case ClassJobType.WhiteMage:
                     return new WhiteMage();
-                case ClassJobType.Dancer:
-                    return new Dancer();
-                case ClassJobType.Gunbreaker:
-                    return new Gunbreaker();
                 default:
                     Logging.Write(Colors.OrangeRed, $@"[ShinraEx] {classJob} is not supported.");
                     return new Default();
